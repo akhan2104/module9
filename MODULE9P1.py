14af988920ce058af16add7a42d38f9921cee515
akhan2104 <123791161+akhan2104@users.noreply.github.com>
2023-03-15 18:35:28 -0500

    Add files via upload

---
+++ MODULE9P1.py
@@ -0,0 +1,4 @@
+s = "True"
+
+while s != "False":
+    print("INFINITE")
---
+++ module 9.py
@@ -0,0 +1,33 @@
+counter  = 0
+L = []
+
+while counter != 11:
+    L.append(counter)
+    counter += 1
+
+print(L)
+
+
+sums= 0
+L2= []
+
+while sums <= 99:
+    number= int(input("Enter a number"))
+    L2.append(number)
+    sums= sum(L2)
+
+print("The sum of L2 is more than or equal to 100")
+    
+
+counter2= 0
+tens=[]
+
+while counter2 != 51:
+    if counter2 % 10 == 0:
+    
+        tens.append(counter2)
+    counter2 += 1
+print(tens)
+
+
+#Done by Mohammed Adnan Khan
